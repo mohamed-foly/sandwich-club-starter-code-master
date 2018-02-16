b18f04b544a2505c14b3d95b82321aff9aa1a151
Mohamed <m.alaoo@yahoo.com>
2018-02-16 10:51:13 +0200

    Json Parsing

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,14 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class JsonUtils {
@@ -12,32 +16,30 @@ public class JsonUtils {
     public static Sandwich parseSandwichJson(String json) {
         try{
             JSONObject jsonObject = new JSONObject(json);
-            String mainName = jsonObject.getString("mainName");
+            JSONObject name = jsonObject.getJSONObject("name");
+            String mainName = name.getString("mainName");
 
-            List<String> alsoKnownAs = null;
-            JSONArray alsoKnownAsJson = jsonObject.getJSONArray("alsoKnownAs");
-            int alsoKnownAsLength = alsoKnownAsJson.length();
-            if (alsoKnownAsLength > 0) {
-                for (int i = 0; i < alsoKnownAsLength; i++) {
-                    alsoKnownAs.add(alsoKnownAsJson.getString(i)) ;
-                }
-            }
+            List<String> alsoKnownAs = JosnTOArray(name.getJSONArray("alsoKnownAs"));
 
             String placeOfOrigin=jsonObject.getString("placeOfOrigin");
             String description=jsonObject.getString("description");
             String image=jsonObject.getString("image");
-            List<String> ingredients = null;
-            JSONArray ingredientsJson = jsonObject.getJSONArray("ingredients");
-            int ingredientsLength = ingredientsJson.length();
-            if (ingredientsLength > 0) {
-                for (int i = 0; i < ingredientsLength; i++) {
-                    ingredients.add(ingredientsJson.getString(i));
-                }
-            }
-            Sandwich sandwich = new Sandwich(mainName,alsoKnownAs,placeOfOrigin,description,image,ingredients);
-            return sandwich;
+            List<String> ingredients =JosnTOArray( jsonObject.getJSONArray("ingredients"));
+            return new Sandwich(mainName,alsoKnownAs,placeOfOrigin,description,image,ingredients);
         }catch (Exception ex){
+            Log.e("Data","EXception : "+ex.getMessage());
             return null;
         }
+    }
+    public static ArrayList<String> JosnTOArray(JSONArray jsonArray) throws JSONException {
+        ArrayList<String> temp = new ArrayList<>();
+        int ingredientsLength = jsonArray.length();
+        if (ingredientsLength > 0) {
+            for (int i = 0; i < ingredientsLength; i++) {
+                temp.add(jsonArray.getString(i));
+            }
+        }
+        return temp;
+
     }
 }
